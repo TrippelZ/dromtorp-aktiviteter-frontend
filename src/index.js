5fba435001e332c390ee2df0ada10cfa3611530c
TrippelZ <zandertrippelzzz@gmail.com>
2023-04-27 13:12:47 +0200

    Loaded colors file. Added routes that will be used.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
+import './colors.css';
 import HomePage from './pages/homePage/homePage';
 import NoPage from './pages/noPage/noPage';
 import Menu from './components/menu/menu';
@@ -13,7 +14,19 @@ root.render(
       <Route path="/" element={<HomePage />} />
       <Route index element={<HomePage />} />
 
-      <Route path="home" element={<HomePage />} />
+      <Route path="/home" element={<HomePage />} />
+
+      <Route path="/user/:userId" element={<NoPage />} />
+      <Route path="/user/:userId/activities" element={<NoPage />} />
+
+      <Route path="/activities" element={<NoPage />} />
+      <Route path="/activities/:activityId" element={<NoPage />} />
+
+      <Route path="/activities/create" element={<NoPage />} />
+      <Route path="/activities/:activityId/configure" element={<NoPage />} />
+
+      <Route path="/login" element={<NoPage />} />
+      <Route path="/register" element={<NoPage />} />
 
       <Route path="*" element={<NoPage />} />
     </Routes>
