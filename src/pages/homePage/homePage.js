c0741228f71f03925face30ab3a0cf58ccf064a8
TrippelZ <zandertrippelzzz@gmail.com>
2023-04-28 14:00:28 +0200

    Added menu to home page. Added so site vavigates to login page if not signed inn.

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -1,10 +1,26 @@
+import { useLoaderData, useNavigate } from 'react-router-dom';
+import Menu from '../../components/menu/menu';
 import './homePage.css';
+import { useEffect } from 'react';
 
 function HomePage() {
+  const navigate    = useNavigate();
+  const sessionInfo = useLoaderData();
+
+  useEffect(() => {
+    if (!sessionInfo.valid) {
+      navigate("/login");
+    }
+  }, [sessionInfo, navigate]);
+
   return (
+    <>
+    <Menu />
+
     <div className="content default-sidebar-margin">
       <h2 className="content-title">Drømtorp Aktiviteter!</h2>
     </div>
+    </>
   );
 }
 
