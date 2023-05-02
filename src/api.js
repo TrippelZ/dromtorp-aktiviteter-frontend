1b1e304ee122198e1e47bd16f63007a2b238f010
TrippelZ <zandertrippelzzz@gmail.com>
2023-05-02 20:34:52 +0200

    Added API call to retrieve a users permission level.

--- src/api.js
+++ src/api.js
@@ -140,3 +140,26 @@ export function GetActivityById(activityID) {
         }
     });
 }
+
+export function GetPermissionLevel(userID) {
+    return new Promise(async (resolve) => {
+        const response = await fetch(API_ADDRESS+"/user/"+userID+"/permissions", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            credentials: "include"
+        });
+
+        try {
+            const data = await response.json();
+
+            resolve(data);
+            return;
+        }
+        catch(error) {
+            resolve();
+            return;
+        }
+    });
+}
