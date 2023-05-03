1b58ae392f83156ae9b4aa9d628e1da9fa759e05
TrippelZ <zandertrippelzzz@gmail.com>
2023-05-04 00:30:20 +0200

    Added functionality to updating name, email and password. Added success response boxes.

--- src/pages/profileEditPage/profileEditPage.js
+++ src/pages/profileEditPage/profileEditPage.js
@@ -1,12 +1,14 @@
-import { useLoaderData, useNavigate } from 'react-router-dom';
+import { useLoaderData, useNavigate, useParams } from 'react-router-dom';
 import Menu from '../../components/menu/menu';
 import './profileEditPage.css';
 import { useEffect, useState } from 'react';
 import { ReactComponent as ReturnIcon } from '../../assets/return.svg';
+import { UpdateUserEmail, UpdateUserName, UpdateUserPassword } from '../../api';
 
 function ProfileEditPage() {
   const navigate    = useNavigate();
   const sessionInfo = useLoaderData();
+  const { userId }  = useParams();
 
   useEffect(() => {
     if (!sessionInfo.valid) {
@@ -14,6 +16,11 @@ function ProfileEditPage() {
     }
   }, [sessionInfo, navigate]);
 
+
+  const [titleFirstName, SetTitleFirstName] = useState(sessionInfo.data.firstName);
+  const [titleLastName , SetTitleLastName ] = useState(sessionInfo.data.lastName);
+
+  const [currentEmail, SetCurrentEmail] = useState(sessionInfo.data.email);
 
   const [firstName      , SetFirstName      ] = useState(sessionInfo.data.firstName);
   const [lastName       , SetLastName       ] = useState(sessionInfo.data.lastName);
@@ -28,61 +35,115 @@ function ProfileEditPage() {
   const [passwordError, SetPasswordError] = useState("");
   const [deleteError  , SetDeleteError  ] = useState("");
 
+  const [nameSuccess    , SetNameSuccess    ] = useState("");
+  const [emailSuccess   , SetEmailSuccess   ] = useState("");
+  const [passwordSuccess, SetPasswordSuccess] = useState("");
+
   async function UpdateName() {
-    if (firstName === "") {
+    const fName = firstName;
+    const lName = lastName;
+
+    if (fName === "") {
+      SetNameSuccess("");
       SetNameError("Mangler fornavn.");
       return;
     }
 
-    if (lastName === "") {
+    if (lName === "") {
+      SetNameSuccess("");
       SetNameError("Mangler etternavn.");
       return;
     }
 
-    if (firstName === sessionInfo.data.firstName && lastName === sessionInfo.data.lastName) {
+    if (fName === titleFirstName && lName === titleLastName) {
+      SetNameSuccess("");
       SetNameError("Ingen forandring.");
       return;
     }
 
-    // api call here
+    const nameChanged = await UpdateUserName(userId, fName, lName);
+
+    if (nameChanged.Error) {
+      SetNameSuccess("");
+      SetNameError(nameChanged.Error);
+      return;
+    }
+
+    SetNameError("");
+    SetNameSuccess("Navn oppdatert.");
+    SetTitleFirstName(fName);
+    SetTitleLastName(lName);
   }
 
   async function UpdateEmail() {
-    if (email === "") {
+    const newEmail = email;
+
+    if (newEmail === "") {
+      SetEmailSuccess("");
       SetEmailError("Mangler epost.");
       return;
     }
 
-    if (email === sessionInfo.data.email) {
+    if (newEmail === currentEmail) {
+      SetEmailSuccess("");
       SetEmailError("Ingen forandring.");
       return;
     }
 
-    // api call here
+    const emailChanged = await UpdateUserEmail(userId, newEmail);
+
+    if (emailChanged.Error) {
+      SetEmailSuccess("");
+      SetEmailError(emailChanged.Error);
+      return;
+    }
+
+    SetEmailError("");
+    SetEmailSuccess("Epost oppdatert.");
+    SetCurrentEmail(newEmail);
   }
 
   async function UpdatePassword() {
-    if (oldPassword === "") {
+    if (oldPassword === "" && !sessionInfo.isAdmin) {
+      SetPasswordSuccess("");
       SetPasswordError("Mangler gammelt passord.");
       return;
     }
 
     if (newPassword === "") {
+      SetPasswordSuccess("");
       SetPasswordError("Mangler nytt passord.");
       return;
     }
 
     if (confirmPassword === "") {
+      SetPasswordSuccess("");
       SetPasswordError("Mangler bekreftelse av nytt passord.");
       return;
     }
 
     if (newPassword !== confirmPassword) {
+      SetPasswordSuccess("");
       SetPasswordError("Nytt passord og bekreftelse er ikke likt.");
       return;
     }
 
-    // api call here
+    const passwordChanged = await UpdateUserPassword(userId, oldPassword, newPassword);
+
+    if (!passwordChanged) {
+      SetPasswordSuccess("");
+      SetPasswordError("Problemer ved oppdatering av passord.");
+      return;
+    }
+    
+    if (passwordChanged.Error) {
+      SetPasswordSuccess("");
+      SetPasswordError(passwordChanged.Error);
+      return;
+    }
+
+    SetPasswordError("");
+    SetPasswordSuccess("Passord oppdatert. Du blir bedt om å logge inn når siden laster på nytt!");
   }
 
   async function DeleteUser() {
@@ -90,6 +151,8 @@ function ProfileEditPage() {
       SetDeleteError("Mangler passord bekreftelse.")
       return;
     }
+
+    SetDeleteError("Ikke ferdig ennå...")
 
     // api call here
   }
@@ -103,7 +166,7 @@ function ProfileEditPage() {
 
         {sessionInfo.data ? <>
         
-          <h2 className="profile-title">{sessionInfo.data.firstName} {sessionInfo.data.lastName}
+          <h2 className="profile-title">{titleFirstName} {titleLastName}
             <ReturnIcon onClick={() => navigate("/user/"+sessionInfo.userId)} className="profile-edit-return-icon" />
           </h2>
           <br />
@@ -113,20 +176,23 @@ function ProfileEditPage() {
           <input type="text" className="profile-lastName-edit" placeholder="Etternavn" value={lastName} onChange={(event) => SetLastName(event.target.value)} />
           <button type="button" onClick={UpdateName}>Oppdater</button>
           <p className="profile-edit-error">{!nameError ? "" : nameError}</p>
+          <p className="profile-edit-success">{!nameSuccess ? "" : nameSuccess}</p>
           
           <br />
           <h3 className="profile-edit-email-title">Epost:</h3>
           <input type="email" className="profile-email-edit" value={email} onChange={(event) => SetEmail(event.target.value)} />
           <button type="button" onClick={UpdateEmail}>Oppdater</button>
           <p className="profile-edit-error">{!emailError ? "" : emailError}</p>
+          <p className="profile-edit-success">{!emailSuccess ? "" : emailSuccess}</p>
 
           <br />
           <h3 className="profile-edit-password-title">Passord:</h3>
-          <input type="password" className="profile-password-edit" placeholder="Gammelt passord" onChange={(event) => SetOldPassword(event.target.value)} />
+          {!sessionInfo.isAdmin && <input type="password" className="profile-password-edit" placeholder="Gammelt passord" onChange={(event) => SetOldPassword(event.target.value)} />}
           <input type="password" className="profile-password-edit" placeholder="Nytt passord" onChange={(event) => SetNewPassword(event.target.value)} />
           <input type="password" className="profile-password-edit" placeholder="Gjenta nytt passord" onChange={(event) => SetConfirmPassword(event.target.value)} />
           <button type="button" onClick={UpdatePassword}>Oppdater</button>
           <p className="profile-edit-error">{!passwordError ? "" : passwordError}</p>
+          <p className="profile-edit-success">{!passwordSuccess ? "" : passwordSuccess}</p>
 
           <br />
           <h3>Slett bruker:</h3>
